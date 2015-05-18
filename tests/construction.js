d41ca15e263bed7073a89b639e886d0023dd53ad
Jasson Casey <jasson@flowgrammable.com>
2015-05-18 17:28:08 -0400

    latest

--- tests/construction.js
+++ tests/construction.js
@@ -4,7 +4,29 @@
 
 var ofp = require('../lib/index');
 
-describe('', function() {
+describe('No throw tests', function() {
+
+  it('OFP 1.0', function() {
+    var msg = ofp['1.0'];
+    msg.Hello();
+    msg.Error();
+    msg.EchoReq();
+    msg.Vendor();
+
+    msg.Hello('aasdfsadf');
+    msg.Error({
+      type: 4,
+      code: 6,
+      data: 'aasdfasdf'
+    });
+    msg.EchoReq('yo');
+    msg.EchoRes('bro');
+    msg.Vendor({
+      id: 1,
+      data: 'secret stuff'
+    });
+  });
+
 });
 
 })();
