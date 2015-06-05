67444d1e5e29d00a3d3fc9448d3b70d201f212f6
Colton Chojnacki <coltonchojnacki@gmail.com>
2015-06-04 21:02:02 -0500

    flowmod init

---
+++ tests/action.js
@@ -0,0 +1,29 @@
+(function(){
+var ofp = require('../lib');
+var uint = require('uint-js');
+var view = ofp.view;
+var a = ofp.action;
+describe('action', function(){
+  it('no throw', function(){
+    a.Output();
+    a.SetVLANVID();
+  });
+
+  it('from view', function(){
+    var buf = new Buffer([0,0,0,8,0,1,0,1]);
+    var v = new view.View(buf);
+    var o = a.fromView(v);
+    var buf2 = new Buffer(buf.length);
+    var v2 = new view.View(buf2);
+    o.toView(v2);
+  });
+
+  it('from view', function(){
+    var buf2 = new Buffer(8);
+    var v2 = new view.View(buf2);
+    var o = a.Output({ port: uint.mk(2, 1), max_len: uint.mk(2, 1) });
+    o.toView(v2);
+  });
+});
+
+})();
